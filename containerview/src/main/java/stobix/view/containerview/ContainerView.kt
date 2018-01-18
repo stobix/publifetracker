be6838d86f8eaddfcdf138613b815281447a6d63
volatile <volatile@localhost>
2018-01-18 15:34:10 +0100

    ContainerView text size settable. Better default color values.

--- containerview/src/main/java/stobix/view/containerview/ContainerView.kt
+++ containerview/src/main/java/stobix/view/containerview/ContainerView.kt
@@ -5,56 +5,63 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.support.v4.content.ContextCompat
+import android.support.v7.appcompat.R.styleable.TextAppearance
+import android.text.style.TextAppearanceSpan
 import android.util.AttributeSet
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 
 open class ContainerView(context: Context,attrs: AttributeSet) : View(context, attrs) {
 
     private var isExternalSet = true
 
+    /*
+    The view needs to tell its parent that it needs to be redrawn if one of these
+    properties gets changed externally.
+    The update function handles this automagically.
+     */
+
     var backColor = 0
-        set(value) {
+        set(value) = update {
             field =value
             fillPaint.color=value
-            invalidateIfExternal()
         }
+
     var textColor: Int = 0
-        set(value) {
+        set(value) = update {
             field =value
             textPaint.color=value
-            invalidateIfExternal()
         }
+
     var rectColorDark: Int = 0
-        set(value) {
+        set(value) = update {
             field =value
             rectColorDarkPaint.color=value
-            invalidateIfExternal()
         }
+
     var rectColorLight: Int = 0
         set(value) = update {
                 field =value
                 rectColorLightPaint.color=value
             }
 
+    var textSize = -1f
+        set(value) = update {
+            field=value
+            if(value>0f)
+                textPaint.textSize = value
+        }
+
+
     private fun update (f:()->Unit) {
         f()
-        invalidateIfExternal()
-    }
-
-    private fun rehashIfExternal(){
-        if(isExternalSet) {
-            invalidate()
-            requestLayout()
-        }
-    }
-
-    private fun invalidateIfExternal(){
         if(isExternalSet) {
             invalidate()
         }
     }
 
+    // properties set in f will not trigger invalidate()
     private fun setInternally(f:()->Unit) {
         isExternalSet=false
         f()
@@ -62,13 +69,6 @@ open class ContainerView(context: Context,attrs: AttributeSet) : View(context, a
     }
 
 
-    var textSize = -1f
-        set(value) {
-            field=value
-            if(value>0f)
-                textPaint.textSize = value
-            invalidateIfExternal()
-        }
 
     private var textPaint = Paint()
     private var fillPaint = Paint()
@@ -79,18 +79,36 @@ open class ContainerView(context: Context,attrs: AttributeSet) : View(context, a
         Log.d("ContainerView init","mupp")
         val a = context.theme.obtainStyledAttributes(attrs, R.styleable.ContainerView,0,0)
 
+
+        fun getDefColor(resource: Int,valueIfNotFound: Int = Color.MAGENTA) : Int {
+            val tv = TypedValue()
+            context.theme.resolveAttribute(resource, tv, true)
+            return if (tv.type >= TypedValue.TYPE_FIRST_COLOR_INT
+                    && tv.type <= TypedValue.TYPE_LAST_COLOR_INT) {
+                // windowBackground is a color
+                tv.data
+            } else {
+                valueIfNotFound
+                // windowBackground is not a color, probably a drawable
+                //Drawable d = activity.getResources().getDrawable(a.resourceId);
+            }
+        }
+
         setInternally {
             backColor = a.getColor(R.styleable.ContainerView_colorBackground,
-                    //ContextCompat.getColor(context, R.color.colorPrimary)
-                    Color.WHITE
-            )
-            textColor = a.getColor(R.styleable.ContainerView_colorText, Color.BLACK)
-            rectColorDark = a.getColor(R.styleable.ContainerView_colorBorderDarkest, Color.DKGRAY)
-            rectColorLight = a.getColor(R.styleable.ContainerView_colorBorderLightest, Color.LTGRAY)
-            if (textSize==-1f)
-                textSize = textPaint.textSize
-            else
-                textPaint.textSize = textSize
+                    getDefColor(android.R.attr.colorPrimary,Color.WHITE))
+            textColor = a.getColor(R.styleable.ContainerView_colorText,
+                   getDefColor(android.R.attr.textColorPrimary,Color.BLACK))
+            rectColorDark = a.getColor(R.styleable.ContainerView_colorBorderDarkest,
+                    getDefColor(android.R.attr.colorAccent,Color.DKGRAY))
+            rectColorLight = a.getColor(R.styleable.ContainerView_colorBorderLightest,
+                    getDefColor(android.R.attr.colorBackground, Color.LTGRAY))
+
+            textSize = a.getDimension(R.styleable.ContainerView_textSize,20f)
+            Log.d("TEXT","size: ${textPaint.textSize}")
+            //textPaint.textSize = TextAppearance.size.toFloat()
+            //Log.d("TEXT","size after: ${textPaint.textSize}")
+            textPaint.textSize = textSize
         }
 
         fillPaint.style=Paint.Style.FILL
@@ -105,7 +123,7 @@ open class ContainerView(context: Context,attrs: AttributeSet) : View(context, a
         super.onDraw(canvas)
         Log.d("ContainerView draw","text size: $textSize")
         canvas.drawColor(backColor)
-        canvas.drawText("test!",10f,10f,textPaint)
+        canvas.drawText("test!",10f,textSize,textPaint)
     }
 
 
