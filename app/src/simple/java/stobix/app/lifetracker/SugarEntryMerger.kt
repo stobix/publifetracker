32b28d9dd8741469412995ebadb1a8413738bdee
Joel Ericson <joel.mikael.ericson@gmail.com>
2018-05-30 12:42:38 +0200

    These names might make code reading easier.

--- app/src/simple/java/stobix/app/lifetracker/SugarEntryMerger.kt
+++ app/src/simple/java/stobix/app/lifetracker/SugarEntryMerger.kt
@@ -1,76 +1,76 @@
 package stobix.app.lifetracker
 
 fun List<SugarEntry>.getMergeables(candidates:List<SugarEntry>) : List<SugarEntry>{
-    val l1 = this.sortedBy { it.epochTimestamp }
-    val l2 = candidates.distinctBy { it.epochTimestamp }.sortedBy { it.epochTimestamp }
+    val selfList = this.sortedBy { it.epochTimestamp }
+    val mergeList = candidates.distinctBy { it.epochTimestamp }.sortedBy { it.epochTimestamp }
 
-    var i1=0
-    var i2=0
+    var iSelfList=0
+    var iMergeList=0
     val acc: MutableList<SugarEntry> = mutableListOf()
 
-    var e1 = l1[0]
-    var t1 = e1.epochTimestamp
-    var e2 = l2[i2]
-    var t2 = e2.epochTimestamp
+    var selfEntry = selfList[0]
+    var selfTimestamp = selfEntry.epochTimestamp
+    var mergeEntry = mergeList[iMergeList]
+    var mergeTimestamp = mergeEntry.epochTimestamp
 
     loop@while(true) {
         when {
-            t1 < t2 -> {
-                i1++
-                if (i1 >= l1.size) {
+            selfTimestamp < mergeTimestamp -> {
+                iSelfList++
+                if (iSelfList >= selfList.size) {
                     // Add all elements with a later timestamp than the list
-                    val remaining = l2.subList(i2,l2.size)
+                    val remaining = mergeList.subList(iMergeList,mergeList.size)
                     fif@if(remaining.size>1) {
                         // we need to check timestamp consistency
-                        var e1 = remaining[0]
-                        // Add micro seconds to all remaining l2 elements until all timestamps are distinct from eachother
-                        consistencyCheck@ for (e2 in remaining.subList(1, remaining.size)) {
+                        var base = remaining[0]
+                        // Add micro seconds to all remaining mergeList elements until all timestamps are distinct from eachother
+                        consistencyCheck@ for (next in remaining.subList(1, remaining.size)) {
                             when {
-                                e1.epochTimestamp < e2.epochTimestamp -> break@consistencyCheck
-                                e1.epochTimestamp == e2.epochTimestamp -> {
-                                    e2.epochTimestamp++
-                                    e1 = e2
+                                base.epochTimestamp < next.epochTimestamp -> break@consistencyCheck
+                                base.epochTimestamp == next.epochTimestamp -> {
+                                    next.epochTimestamp++
+                                    base = next
                                 }
-                                e1.epochTimestamp > e2.epochTimestamp -> continue@consistencyCheck
+                                base.epochTimestamp > next.epochTimestamp -> continue@consistencyCheck
                             }
                         }
                     }
                     acc.addAll(remaining)
                     break@loop
                 } else {
-                    e1 = l1[i1]
-                    t1 = e1.epochTimestamp
+                    selfEntry = selfList[iSelfList]
+                    selfTimestamp = selfEntry.epochTimestamp
                     continue@loop
                 }
             }
-            t1 == t2->
-                if(e1 sameAs e2) {
+            selfTimestamp == mergeTimestamp->
+                if(selfEntry sameAs mergeEntry) {
                     // Discard elements that are the same
-                    i2++
-                    if( i2 >= l2.size) {
+                    iMergeList++
+                    if( iMergeList >= mergeList.size) {
                         break@loop
                     } else {
-                        e2 = l2[i2]
-                        t2 = e2.epochTimestamp
+                        mergeEntry = mergeList[iMergeList]
+                        mergeTimestamp = mergeEntry.epochTimestamp
                         continue@loop
                     }
                     // Make distinct elements that share the same timestamp
                     // This assumes that timestamps that occur at the same microsecond can
                     // be shifted forward a microsecond without breaking anything
                 } else {
-                    e2.epochTimestamp++
-                    t2=e2.epochTimestamp
+                    mergeEntry.epochTimestamp++
+                    mergeTimestamp=mergeEntry.epochTimestamp
                     continue@loop
                 }
-            t1 > t2-> {
+            selfTimestamp > mergeTimestamp-> {
                 // Add elements with an earlier timestamp than the list
-                acc.plusAssign(e2)
-                i2++
-                if( i2 >= l2.size) {
+                acc.plusAssign(mergeEntry)
+                iMergeList++
+                if( iMergeList >= mergeList.size) {
                     break@loop
                 } else {
-                    e2 = l2[i2]
-                    t2 = e2.epochTimestamp
+                    mergeEntry = mergeList[iMergeList]
+                    mergeTimestamp = mergeEntry.epochTimestamp
                     continue@loop
                 }
             }
