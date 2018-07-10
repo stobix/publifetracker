4cccc66baf3ae33644c848ac2a9f0dfae17f9b2c
Joel Ericson <joel.mikael.ericson@gmail.com>
2018-07-10 14:42:52 +0200

    Went over the old TODO's laying around.

--- app/src/simple/java/stobix/app/lifetracker/SugarEntryDao.java
+++ app/src/simple/java/stobix/app/lifetracker/SugarEntryDao.java
@@ -36,9 +36,7 @@ public interface SugarEntryDao {
     @Query("select * from sugar_entries where sugar > -1 order by timestamp")
     List<SugarEntry> getAllSugarPoints();
 
-    /* TODO Maybe make some kind of converter so that I never return whole sugar entries for the graphs
-    */
-    @Query("select timestamp, sugar as value from sugar_entries where sugar > -1 order by timestamp")
+    @Query("select timestamp, sugar as value from sugar_entries where sugar > 0 order by timestamp")
     List<FloatyIntBucket> getAllSugarBuckets();
 
     @Query("select timestamp, weight as value from sugar_entries where weight > 0 order by timestamp") // For some reason, "where weight != null" returned zero results always
