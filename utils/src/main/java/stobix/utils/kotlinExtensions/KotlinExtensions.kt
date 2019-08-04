d39fa99a167d03cb6dbf8d508a63a207b95c1b67
Joel Ericson <joel.mikael.ericson@gmail.com>
2019-08-04 22:58:13 +0200

    map and fold over tuplets. Code rearrangement for legibility.

--- utils/src/main/java/stobix/utils/kotlinExtensions/KotlinExtensions.kt
+++ utils/src/main/java/stobix/utils/kotlinExtensions/KotlinExtensions.kt
@@ -22,10 +22,19 @@ data class Quadruple<out A,out B,out C,out D> (val first:A, val second:B, val th
     override fun toString(): String = "($first, $second, $third, $fourth)"
 }
 
-fun <T> Quadruple<T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth)
+data class Quintuple<out A,out B,out C,out D, out E> (val first:A, val second:B, val third:C, val fourth:D, val fifth:E): Serializable {
+    override fun toString(): String = "($first, $second, $third, $fourth, $fifth)"
+}
 
-fun <A,B> Quadruple<A,A,A,A>.map(f:(A)->B) =
-        f(first) to f(second) to f(third) to f(fourth)
+data class Sextuple<out A,out B,out C,out D, out E, out F> (val first:A, val second:B, val third:C, val fourth:D, val fifth:E, val sixth:F): Serializable {
+    override fun toString(): String = "($first, $second, $third, $fourth, $fifth)"
+}
+
+fun <A,B,C> Pair<A,A>.zipWith(q: Pair<B,B>, f:(A,B)->C) =
+        f(first,q.first) to f(second,q.second)
+
+fun <A,B,C> Triple<A,A,A>.zipWith(q: Triple<B,B,B>, f:(A,B)->C) =
+        f(first,q.first) to f(second,q.second) to f(third,q.third)
 
 fun <A,B,C> Quadruple<A,A,A,A>.zipWith(q: Quadruple<B,B,B,B>, f:(A,B)->C) =
         f(first,q.first) to f(second,q.second) to f(third,q.third) to f(fourth,q.fourth)
@@ -33,30 +42,23 @@ fun <A,B,C> Quadruple<A,A,A,A>.zipWith(q: Quadruple<B,B,B,B>, f:(A,B)->C) =
 fun <A,B,C> Quadruple<A,A,A,A>.zipWith(f:(A,B)->C, q: Quadruple<B,B,B,B>) =
         f(first,q.first) to f(second,q.second) to f(third,q.third) to f(fourth,q.fourth)
 
-operator fun Quadruple<Int,Int,Int,Int>.plus(q:Quadruple<Int,Int,Int,Int>)=
-        this.zipWith(q) {a,b -> a+b}
 
-operator fun Quadruple<Int,Int,Int,Int>.minus(q:Quadruple<Int,Int,Int,Int>)=
-        this.zipWith(q) {a,b -> a-b}
-
-operator fun Quadruple<Float,Float,Float,Float>.times(q:Quadruple<Float,Float,Float,Float>)  = this.zipWith(q) {a,b -> a*b}
-
-operator fun Quadruple<Int,Int,Int,Int>.div(dividend: Int)=
-        this.map { it / dividend }
-
-operator fun Quadruple<Int,Int,Int,Int>.div(dividend: Float)=
-        this.map { it.toFloat() / dividend }
-
-
-data class Quintuple<out A,out B,out C,out D, out E> (val first:A, val second:B, val third:C, val fourth:D, val fifth:E): Serializable {
-    override fun toString(): String = "($first, $second, $third, $fourth, $fifth)"
-}
-
+fun <T> Triple<T, T, T>.toList(): List<T> = listOf(first, second, third)
+fun <T> Quadruple<T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth)
 fun <T> Quintuple<T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth)
-
-data class Sextuple<out A,out B,out C,out D, out E, out F> (val first:A, val second:B, val third:C, val fourth:D, val fifth:E, val sixth:F): Serializable {
-    override fun toString(): String = "($first, $second, $third, $fourth, $fifth)"
-
-}
-
 fun <T> Sextuple<T, T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth, sixth)
+
+fun <A,B> Pair<A,A>.map(f:(A)->B) = f(first) to f(second)
+fun <A,B> Triple<A,A,A>.map(f:(A)->B) = f(first) to f(second) to f(third)
+fun <A,B> Quadruple<A,A,A,A>.map(f:(A)->B) = f(first) to f(second) to f(third) to f(fourth)
+fun <A,B> Quintuple<A,A,A,A,A>.map(f:(A)->B) = f(first) to f(second) to f(third) to f(fourth) to f(fifth)
+fun <A,B> Sextuple<A,A,A,A,A,A>.map(f:(A)->B) = f(first) to f(second) to f(third) to f(fourth) to f(fifth) to f(sixth)
+
+infix fun <A,B> Pair<A,A>.folding(f:(A,A) -> B) = f(first,second)
+infix fun <A> Triple<A,A,A>.folding(f:(A,A) -> A) = first to second folding f to third folding f
+infix fun <A> Quadruple<A,A,A,A>.folding(f:(A,A) -> A) = first to second folding f to third folding f to fourth folding f
+infix fun <A> Quintuple<A,A,A,A,A>.folding(f:(A,A) -> A) = first to second folding f to third folding f to fourth folding f to fifth folding f
+infix fun <A> Sextuple<A,A,A,A,A,A>.folding(f:(A,A) -> A) = first to second folding f to third folding f to fourth folding f to fifth folding f to sixth folding f
+
+
+
