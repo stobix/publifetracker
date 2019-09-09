69d14345af514ff0ea1ddf656d9e1f391e30b9a3
Joel Ericson <joel.mikael.ericson@gmail.com>
2019-09-09 20:45:24 +0200

    Better sleep action icon. Added time interval length view.

--- app/src/simple/java/stobix/app/lifetracker/SugarEntryTableDataAdapter.kt
+++ app/src/simple/java/stobix/app/lifetracker/SugarEntryTableDataAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import de.codecrafters.tableview.TableDataAdapter
+import stobix.utils.DateHandler
 import java.util.*
 
 /**
@@ -21,31 +22,50 @@ class SugarEntryTableDataAdapter(
         entries: ArrayList<SugarEntry>
 ) : TableDataAdapter<SugarEntry>(context, entries) {
 
-    private infix fun<A> A.pairedIfDefined(b: String?) = b?.let { this to it }
+    private infix fun <A> A.pairedIfDefined(b: String?) = b?.let { this to it }
 
     override fun getCellView(rowIndex: Int, columnIndex: Int, parentView: ViewGroup): View {
         val currRow = getRowData(rowIndex)
         val formatString = resources.getString(R.string.dateTimeFormat)
+        val timeFormatString = resources.getString(R.string.timeFormat)
+        fun endTimeFormat() = currRow.endTimestamp?.let {
+            val totMilSeconds = (it - currRow.timestamp)
+            val totSeconds = totMilSeconds / 1000
+            val seconds = (totSeconds % 60).toInt()
+            val minutes = ((totSeconds / 60) % 60).toInt()
+            var hours = ((totSeconds / 3600) % 60).toInt()
+            val days = (totSeconds / 3600 / 24).toInt()
+            if (days > 0)
+                "%d:%02d:%02d:%02d".format(days, hours, minutes, seconds)
+            else if (hours > 0)
+                "%d:%02d:%02d".format(hours, minutes, seconds)
+            else if (minutes > 0)
+                "%d:%02d".format(minutes, seconds)
+            else
+                "%ds".format(seconds)
+            //"($days:$hours:$minutes:$seconds)"
+            // DateFormat.format(timeFormatString,sumDate) TODO make that work
+        }
         return when (columnIndex) {
             0 -> {
                 val startDate = Date(currRow.timestamp)
                 val startString = DateFormat.format(formatString, startDate).toString()
-                val totalString = currRow.endTimestamp?.let{
+                val totalString = currRow.endTimestamp?.let {
                     val date = Date(it)
-                    "$startString -\n"+DateFormat.format(formatString,date).toString()
+                    "$startString -\n" + DateFormat.format(formatString, date).toString()
                 } ?: startString
                 renderString(totalString)
             }
 
             1 ->
-                if((context as MainActivity).displaysListIcons())
+                if ((context as MainActivity).displaysListIcons())
                     renderStringsWithIcons(
                             /*R.drawable.datetime_icon pairedIfDefined currRow.endTimestamp?.let{
                                 val date = Date(it)
                                 DateFormat.format(formatString,date).toString()
                             }
                             ,*/
-                            R.drawable.blood_sugar_icon pairedIfDefined currRow.sugarLevel?.let { String.format("%.1f mmol/l", it / 10f)}
+                            R.drawable.blood_sugar_icon pairedIfDefined currRow.sugarLevel?.let { String.format("%.1f mmol/l", it / 10f) }
                             ,
                             R.drawable.weight_icon pairedIfDefined currRow.weight?.let { String.format("%.1f kg", it / 10f) }
                             ,
@@ -53,15 +73,18 @@ class SugarEntryTableDataAdapter(
                             ,
                             R.drawable.drink_icon pairedIfDefined currRow.drink
                             ,
-                            R.drawable.treatment_icon pairedIfDefined currRow.insulin ?.let { String.format("%.1f insulin", it )}
+                            R.drawable.treatment_icon pairedIfDefined currRow.insulin?.let { String.format("%.1f insulin", it) }
                             ,
                             R.drawable.pills_icon pairedIfDefined currRow.treatment
                             ,
                             R.drawable.extra_icon pairedIfDefined currRow.extra
+                            ,
+                            R.drawable.datetime_icon pairedIfDefined endTimeFormat()
+
                     )
                 else
                     renderStrings(parentView,
-                            currRow.sugarLevel?.let { String.format("%.1f mmol/l", it / 10f)}
+                            currRow.sugarLevel?.let { String.format("%.1f mmol/l", it / 10f) }
                             ,
                             currRow.weight?.let { String.format("%.1f kg", it / 10f) }
                             ,
@@ -69,33 +92,35 @@ class SugarEntryTableDataAdapter(
                             ,
                             currRow.drink
                             ,
-                            currRow.insulin ?.let { String.format("%.1f insulin", it )}
+                            currRow.insulin?.let { String.format("%.1f insulin", it) }
                             ,
                             currRow.treatment
                             ,
                             currRow.extra
+                            ,
+                            endTimeFormat()?.let { "($it)" }
                     )
             else -> renderString("N/A")
         }
     }
 
-    private fun renderStringsWithIcons(vararg values: Pair<Int,String>?): View{
+    private fun renderStringsWithIcons(vararg values: Pair<Int, String>?): View {
         val compositeView = LinearLayout(context)
         compositeView.orientation = LinearLayout.VERTICAL
         val metrics = (context as MainActivity).screenMetrics
         values.filterNotNull().forEach {
-            if (it.second!="" ) {
+            if (it.second != "") {
                 val textView = renderString(it.second)
                 // Needed to get the text height
-                textView.measure(metrics.widthPixels,0)
-                val imageSideLen =  textView.measuredHeight.times(1.3).toInt()
+                textView.measure(metrics.widthPixels, 0)
+                val imageSideLen = textView.measuredHeight.times(1.3).toInt()
 
                 val iconView = ImageView(context)
                 iconView.setImageResource(it.first)
                 iconView.setPadding(20, 10, 20, 10)
 
                 // Set the size of the image view
-                val iconParams = LinearLayout.LayoutParams(imageSideLen,imageSideLen)
+                val iconParams = LinearLayout.LayoutParams(imageSideLen, imageSideLen)
                 iconView.layoutParams = iconParams
                 iconView.maxHeight = imageSideLen
 
@@ -109,25 +134,25 @@ class SugarEntryTableDataAdapter(
         return compositeView
     }
 
-    private fun renderStrings(parentView: ViewGroup,vararg values: String?): View{
+    private fun renderStrings(parentView: ViewGroup, vararg values: String?): View {
         val compositeView = LinearLayout(context)
         compositeView.orientation = LinearLayout.VERTICAL
         var horizView = LinearLayout(context)
 
-        parentView.measure(0,0)
+        parentView.measure(0, 0)
         val prevWidth = parentView.measuredWidth
         val maxWidth = (context as MainActivity).screenMetrics.widthPixels - prevWidth
         var sumWidth = 0
         values.filterNotNull().forEach {
-            if (it!="" ) {
+            if (it != "") {
                 val textView = renderString(it)
-                textView.measure(0,0)
-                if( textView.measuredWidth+sumWidth < maxWidth ){
-                    Log.d("widths","${textView.measuredWidth}+$sumWidth < $maxWidth")
-                    horizView.addView( textView )
-                    sumWidth+=textView.measuredWidth
+                textView.measure(0, 0)
+                if (textView.measuredWidth + sumWidth < maxWidth) {
+                    Log.d("widths", "${textView.measuredWidth}+$sumWidth < $maxWidth")
+                    horizView.addView(textView)
+                    sumWidth += textView.measuredWidth
                 } else {
-                    Log.d("widths","${textView.measuredWidth}+$sumWidth > $maxWidth")
+                    Log.d("widths", "${textView.measuredWidth}+$sumWidth > $maxWidth")
                     compositeView.addView(horizView)
                     horizView = LinearLayout(context)
                     horizView.addView(textView)
